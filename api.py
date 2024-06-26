81072fb09b5a7962c10c075553ffeaceba33a27e
sa191711490 <92499638+sa191711490@users.noreply.github.com>
2024-06-26 23:47:15 +0530

    hist

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from multidst.functions import multitest
-import random
+from multidst.utils.visualization import multidst_hist
 
 class P_Values(BaseModel):
     p_values: str
@@ -17,11 +17,14 @@ async def root():
 #send p values and methods
 @app.post("/analyze")
 async def analyze(request:P_Values):
-    #number_tuple = [random.uniform(0,0.03) for i in range(1000)]
-    #p_values = "0.0000085,0.000023,0.000152,0.0016024,0.0040207,0.02212,0.024306,0.032078,0.036851"
     number_strings = request.p_values.split(',')
     number_list = [float(num) for num in number_strings]
     number_tuple = tuple(number_list)
+    #Histogram
+    g2_index = []
+    multidst_hist(number_tuple, g2_index, title="Histogram of p-values",col1 = 'skyblue', col2 = 'purple')
+    #sigplot
+    
     # Carry out MultiDST for a list of p_values
     res = multitest(number_tuple, alpha=0.05, sigplot=False)
     if isinstance(res, dict):
