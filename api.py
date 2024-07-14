f46fb4b8a803be5ee282a6e4914f2e098846cb8d
sa191711490 <92499638+sa191711490@users.noreply.github.com>
2024-07-14 23:59:36 +0530

    unique id

--- api.py
+++ api.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from multidst.functions import multitest
 from multidst.utils.visualization import multidst_hist , sigindex_plot
-
+import time 
 class P_Values(BaseModel):
     p_values: str
 
 app = FastAPI()
 
-
+# to run the uvicorn dev server uvicorn api:app
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -20,14 +20,16 @@ async def analyze(request:P_Values):
     number_strings = request.p_values.split(',')
     number_list = [float(num) for num in number_strings]
     number_tuple = tuple(number_list)
+    #unique_plot_name
+    ts = str(time.time())
     #Histogram
     g2_index = []
-    multidst_hist(number_tuple, g2_index, title="Histogram of p-values",col1 = 'skyblue', col2 = 'purple')
+    multidst_hist(number_tuple, g2_index, title="Histogram of p-values",col1 = 'skyblue', col2 = 'purple', save_plot=True, timestamp=ts)
     #sigplot
     methods = ['Bonferroni', 'Holm', 'SGoF', 'BH', 'BY', 'Q value']
-    res = multitest(number_tuple, alpha=0.05, sigplot=True)
+    res = multitest(number_tuple, alpha=0.05)
     sig_indices = [res['Bonferroni'], res['Holm'], res['SGoF'], res['BH'], res['BY'], res['Q-value']]
-    sigindex_plot(methods, sig_indices, title=None, save_plot=True) 
+    sigindex_plot(methods, sig_indices, title=None, save_plot=True,timestamp=ts) 
     # Carry out MultiDST for a list of p_values
     res = multitest(number_tuple, alpha=0.05, sigplot=False)
     if isinstance(res, dict):
@@ -37,7 +39,8 @@ async def analyze(request:P_Values):
             "SGoF": res["SGoF"],
             "BH": res["BH"],
             "BY": res["BY"],
-            "Q-value": res["Q-value"]
+            "Q-value": res["Q-value"],
+            "time":ts
         }
     else:
         return {"error": "Unexpected result format from multitest"}
